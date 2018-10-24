3f3ec280e08861a6574ec6d68cc08387d19b34e1
henrix mart <henrixmart@gmail.com>
2018-10-24 15:42:09 +0800

    recipe content

--- src/app/views/user-view/user-view.component.ts
+++ src/app/views/user-view/user-view.component.ts
@@ -7,11 +7,23 @@ class TagRates {
   avgrate: number;
   totalrate: number;
   totalpost: number;
+  constructor() {
+    this.tagname = faker.lorem.word();
+    this.avgrate = faker.random.number(5);
+    this.totalrate = faker.random.number(100);
+    this.totalpost = faker.random.number(1000);
+  }
 }
 class VRecipe {
   id: number;
   name: string;
   description: string;
+
+  constructor() {
+    this.id = faker.random.number(10);
+    this.name = faker.name.title();
+    this.description = faker.lorem.sentences(5);
+  }
 }
 @Component({
   selector: 'app-user-view',
@@ -35,6 +47,26 @@ export class UserViewComponent implements OnInit {
     this.bio = faker.lorem.paragraphs(5, '\n');
     this.avgrating = faker.random.number(20) / 10 + 3;
     this.totalrating = faker.random.number(1000) + 1000;
+    this.datejoined = faker.date.recent(23);
+    this.toprecipes = [
+      new VRecipe(),
+      new VRecipe(),
+      new VRecipe(),
+      new VRecipe(),
+      new VRecipe(),
+    ];
+    this.contributionstag = [
+      new TagRates(),
+      new TagRates(),
+      new TagRates(),
+      new TagRates(),
+      new TagRates(),
+      new TagRates(),
+      new TagRates(),
+      new TagRates(),
+      new TagRates(),
+      new TagRates()
+    ];
   }
 
   ngOnInit() {
